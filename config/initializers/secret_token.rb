1f56e8190dfcf365a25b34faee91d3b19067a614
Adeel Khan <adeelnkhan@gmail.com>
2014-10-06 21:30:09 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Artzone::Application.config.secret_key_base = '138a0556b839e10aee5979891b5060d603cbd4a5eaee8e3c53e3bb92306f44bdfc8e3d485092eb66fc7e1ce4c199a17647560d6d4ff638e51e313a9ec00a580f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Artzone::Application.config.session_store :cookie_store, key: '_artzone_session'
