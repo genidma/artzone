a1c38d3a17ecda864c73db71b6c105d0de810ccb
Adeel Khan <adeelnkhan@gmail.com>
2014-10-06 21:41:07 +0000

    hooked up the static page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Artzone::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'static_pages#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -53,4 +53,6 @@ Artzone::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+
 end
